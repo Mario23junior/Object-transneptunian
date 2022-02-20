81bfd2a181fa22d27f14984dbde61ff9c9b10fe8
Mario23junior <mariojunior3251@gmail.com>
2022-02-20 05:13:56 -0300

    Criando modelo da base de dados de transnetunianos

---
+++ src/main/java/com/br/api/transnetunianos/model/Transnetunianos.java
@@ -0,0 +1,47 @@
+package com.br.api.transnetunianos.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Transnetunianos {
+  
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	private Integer numero;
+	private String nome;
+	private Double designacaoProvi;
+	
+	public Transnetunianos() {
+	}
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public Integer getNumero() {
+		return numero;
+	}
+	public void setNumero(Integer numero) {
+		this.numero = numero;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public Double getDesignacaoProvi() {
+		return designacaoProvi;
+	}
+	public void setDesignacaoProvi(Double designacaoProvi) {
+		this.designacaoProvi = designacaoProvi;
+	}
+	
+	
+}
