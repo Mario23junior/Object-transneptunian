936310b66763b08aa10c8629db1be9d16b9ef4b4
Mario23junior <mariojunior3251@gmail.com>
2022-02-20 20:40:33 -0300

    Criando repositry de entidades

---
+++ src/main/java/com/br/api/transnetunianos/repsitory/CendenciaInforRepository.java
@@ -0,0 +1,9 @@
+package com.br.api.transnetunianos.repsitory;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.br.api.transnetunianos.model.CendenciaInformat;
+
+public interface CendenciaInforRepository extends JpaRepository<CendenciaInformat, Long>{
+
+}
---
+++ src/main/java/com/br/api/transnetunianos/repsitory/DesignacaoRepository.java
@@ -0,0 +1,9 @@
+package com.br.api.transnetunianos.repsitory;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.br.api.transnetunianos.model.Designacao;
+
+public interface DesignacaoRepository extends JpaRepository<Designacao, Long>{
+
+}
