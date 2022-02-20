119f75a1c75110a84b2acb8fb11a2d8c5e318f23
Mario23junior <mariojunior3251@gmail.com>
2022-02-20 04:52:46 -0300

    Iniciando api de objetos em volta do eixo do sol

---
+++ src/main/java/com/br/api/transnetunianos/TransnetunianosApplication.java
@@ -0,0 +1,13 @@
+package com.br.api.transnetunianos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransnetunianosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransnetunianosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/api/transnetunianos/TransnetunianosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.api.transnetunianos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransnetunianosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
